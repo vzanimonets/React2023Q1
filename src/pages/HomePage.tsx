ceabd14037ead965e9e0ad403b8656daf949c169
Viktar Zanimonets <vzanimonets@gmail.com>
2023-03-18 19:08:29 +0300

    feat: Home page implementation

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { ItemType } from '../components/App/App';
+import styles from '../components/Item/item.module.css';
 import SearchBar from '../components/SearchBar/SearchBar';
+import Item from '../components/Item/Item';
 
 type PropsType = {
   data: ItemType[];
@@ -11,6 +13,15 @@ const HomePage = (props: PropsType) => {
   return (
     <>
       <SearchBar />
+      <ul className={styles.cards}>
+        {data.map((item) => {
+          return (
+            <li className={styles.cards__item} key={item.id}>
+              <Item {...item} />
+            </li>
+          );
+        })}
+      </ul>
     </>
   );
 };
