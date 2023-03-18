ed6709778552dd2f66f093546f406ac45a83d31e
Viktar Zanimonets <vzanimonets@gmail.com>
2023-03-18 18:53:25 +0300

    feat: Add fake data for MainPage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,17 @@
 import React from 'react';
+import { ItemType } from '../components/App/App';
+import SearchBar from '../components/SearchBar/SearchBar';
 
-const HomePage = () => {
-  return <>HomePage component</>;
+type PropsType = {
+  data: ItemType[];
+};
+
+const HomePage = (props: PropsType) => {
+  const { data } = props;
+  return (
+    <>
+      <SearchBar />
+    </>
+  );
 };
 export default HomePage;
