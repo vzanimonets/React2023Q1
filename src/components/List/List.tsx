7cc740e3303ff6a309ad03fe9624b3f896dd92c4
Viktar Zanimonets <vzanimonets@gmail.com>
2023-03-23 14:44:53 +0300

    feat: Add List component

---
+++ src/components/List/List.tsx
@@ -0,0 +1,26 @@
+import styles from './list.module.css';
+import Item from '../Item/Item';
+import React from 'react';
+import { ItemType } from '../App/App';
+
+type PropsType = {
+  data: ItemType[];
+};
+
+const List = ({ data }: PropsType) => {
+  /**
+   * TODO: implement key generation
+   */
+  return (
+    <ul className={styles.cards}>
+      {data.map((item,i) => {
+        return (
+          <li className={styles.cards__item} key={i}>
+            <Item {...item} />
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+export default List;
