dc70bce81d72e02968944ef13cec7802db53371d
Viktar Zanimonets <vzanimonets@gmail.com>
2023-03-30 14:01:13 +0300

    feat: rewrite SearchBar to the stateless functional component

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,47 +1,43 @@
-import React, { createRef } from 'react';
+import React, { FormEvent, useEffect, useRef } from 'react';
 import styles from './search.module.css';
 
-class SearchBar extends React.PureComponent {
-  state = {
-    initValue:
-      typeof localStorage.getItem('searchVal') !== undefined
-        ? localStorage.getItem('searchVal')
-        : '',
-  };
-  private inputRef = createRef<HTMLInputElement>();
+const SearchBar = () => {
+  const initValue = !!localStorage.getItem('searchVal') ? localStorage.getItem('searchVal') : '';
+  const inputRef = useRef<HTMLInputElement>(null);
 
-  saveToStorage = () => {
-    const value = JSON.stringify(this.inputRef.current?.value);
-    localStorage.setItem('searchVal', value);
+  const saveToStorage = (e: FormEvent) => {
+    e.preventDefault();
   };
 
-  componentWillUnmount = () => {
-    this.saveToStorage();
-  };
+  useEffect(() => {
+    const valueRef = { current: inputRef.current };
+    return () => {
+      const value = JSON.stringify(valueRef.current?.value);
+      localStorage.setItem('searchVal', value);
+    };
+  }, []);
 
-  render() {
-    return (
-      <div className={styles.searchBox}>
-        <form action="/" onSubmit={this.saveToStorage}>
-          <div className={styles.search__input}>
-            <button type="submit"></button>
-            <input
-              defaultValue={this.state.initValue ? JSON.parse(this.state.initValue) : ''}
-              type="search"
-              placeholder="Search..."
-              ref={this.inputRef}
-            />
-          </div>
-          <div className={styles.radio}>
-            <input type="radio" id="searchChoice1" name="search" value="site" defaultChecked />
-            <label htmlFor="searchChoice1">Search in domain.com</label>
-            <input type="radio" id="searchChoice2" name="search" value="web" />
-            <label htmlFor="searchChoice2">Search in web</label>
-          </div>
-        </form>
-      </div>
-    );
-  }
-}
+  return (
+    <div className={styles.searchBox}>
+      <form onSubmit={saveToStorage}>
+        <div className={styles.search__input}>
+          <button type="submit"></button>
+          <input
+            defaultValue={initValue && JSON.parse(initValue)}
+            type="search"
+            placeholder="Search..."
+            ref={inputRef}
+          />
+        </div>
+        <div className={styles.radio}>
+          <input type="radio" id="searchChoice1" name="search" value="site" defaultChecked />
+          <label htmlFor="searchChoice1">Search in domain.com</label>
+          <input type="radio" id="searchChoice2" name="search" value="web" />
+          <label htmlFor="searchChoice2">Search in web</label>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default SearchBar;
