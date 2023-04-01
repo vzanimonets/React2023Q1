1263c9f496525891ab446a9c77ad85b88df8241a
Viktar Zanimonets <vzanimonets@gmail.com>
2023-04-01 21:06:30 +0300

    feat: create LabeledSelect component

---
+++ src/components/LabeledSelect/LabeledSelect.tsx
@@ -0,0 +1,39 @@
+import React, { FC, SelectHTMLAttributes } from 'react';
+import { RegisterOptions, UseFormRegister } from 'react-hook-form';
+import styles from '../Form/form.module.css';
+import { FormFieldsType, validateFields } from '../Form/Form';
+
+interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
+  register: UseFormRegister<validateFields>;
+  name: FormFieldsType;
+  label: string;
+  options: { value: string; label: string }[];
+  rules?: RegisterOptions;
+  errors?: string;
+}
+
+const LabeledSelect: FC<SelectProps> = ({
+  register,
+  name,
+  label,
+  options,
+  rules,
+  errors,
+  ...rest
+}) => {
+  return (
+    <>
+      <label>{label}</label>
+      <select {...(register && register(name, rules))} {...rest}>
+        {options.map((option, i) => (
+          <option value={option.value} key={`select_${option.value}_${i}`}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+      {errors && <span className={styles.error__message}>{errors}</span>}
+    </>
+  );
+};
+
+export default LabeledSelect;
