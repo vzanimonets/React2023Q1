c8294e43aad3e72c38eb06582593040388f2241f
Viktar Zanimonets <vzanimonets@gmail.com>
2023-04-01 12:05:30 +0300

    hotfix: Item test's fixes

--- src/__tests__/Item.test.tsx
+++ src/__tests__/Item.test.tsx
@@ -8,11 +8,15 @@ describe('Item test', () => {
     const item = {
       id: uuidv4(),
       title: 'Item title',
-      text: 'Item description',
+      description: 'Item description',
       image: 'https://unsplash.it/800/600?image=82',
+      status: 'awaiting',
+      delivery: 'No',
     };
     render(<Item {...item} />);
     expect(screen.getByText(/Item title/i)).toBeDefined();
     expect(screen.getByText(/Item description/i)).toBeDefined();
+    expect(screen.getByText(/awaiting/i)).toBeDefined();
+    expect(screen.getByText(/No/i)).toBeDefined();
   });
 });
