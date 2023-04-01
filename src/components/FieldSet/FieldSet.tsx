cd927c3fe6672f9f6b9b7654cd0e13edfa25382f
Viktar Zanimonets <vzanimonets@gmail.com>
2023-04-01 21:04:47 +0300

    feat: create FieldSet component

---
+++ src/components/FieldSet/FieldSet.tsx
@@ -0,0 +1,18 @@
+import { FC, FieldsetHTMLAttributes, ReactNode } from 'react';
+import styles from '../Form/form.module.css';
+import React from 'react';
+
+interface FieldsetProps extends FieldsetHTMLAttributes<HTMLFieldSetElement> {
+  errors?: string;
+  children: ReactNode;
+}
+
+const FieldSet: FC<FieldsetProps> = ({ errors, children, ...rest }) => {
+  return (
+    <fieldset className={errors ? styles.hasError : ''} {...rest}>
+      {children}
+    </fieldset>
+  );
+};
+
+export default FieldSet;
