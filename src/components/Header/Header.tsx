7b10a07dd09b0f61e909dccc0c271f14af977339
Viktar Zanimonets <vzanimonets@gmail.com>
2023-04-02 18:32:27 +0300

    hotfix: move useLocation hook to Layout component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,20 +1,10 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
 
-const Header = () => {
-  const location = useLocation();
-  const getTitle = () => {
-    switch (location.pathname) {
-      case '/':
-        return 'Main page';
-      case '/about':
-        return 'About page';
-      case '/forms':
-        return 'Forms page';
-      default:
-        return '';
-    }
-  };
-  return <h2>{getTitle()}</h2>;
+type PropsType = {
+  pageTitle: string;
+};
+
+const Header = ({ pageTitle }: PropsType) => {
+  return <h2>{pageTitle}</h2>;
 };
 export default Header;
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Navigation from '../Navigation/Navigation';
 import Header from '../Header/Header';
 
 const Layout = () => {
+  const location = useLocation();
+  const getTitle = () => {
+    switch (location.pathname) {
+      case '/':
+        return 'Main page';
+      case '/about':
+        return 'About page';
+      case '/forms':
+        return 'Forms page';
+      default:
+        return '';
+    }
+  };
+  const pageTitle = getTitle();
   return (
     <>
-      <Header />
+      <Header pageTitle={pageTitle} />
       <Navigation />
       <main>
         <Outlet />
