f0230fc5d658f64e5f29a479890f5b7e7d4a8d15
Viktar Zanimonets <vzanimonets@gmail.com>
2023-04-07 15:38:50 +0300

    feat: implement API request(getAll) for Homepage component

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,23 +1,42 @@
-import React, { useEffect, useState } from 'react';
-import { ItemType } from '../components/App/App';
+import React, { FC, useEffect, useState } from 'react';
+import { createPortal } from 'react-dom';
 import SearchBar from '../components/SearchBar/SearchBar';
 import List from '../components/List/List';
+import Modal from '../components/Modal/Modal';
+import { ShotInfoType } from '../components/App/App';
+import { DataAPI } from '../services/dataAPI';
 
-type PropsType = {
-  data: ItemType[];
-};
-
-const HomePage = ({ data }: PropsType) => {
-  const [items, setItems] = useState<ItemType[]>([]);
+const HomePage: FC = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [items, setItems] = useState<ShotInfoType[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    setItems([...data]);
-  }, [data]);
+    (async () => {
+      const params = { fields: 'firstName,lastName,age,image' };
+      setIsLoading(true);
+      const data = await DataAPI.getAll(params);
+      setIsLoading(false);
+      setItems((prevState) => [...prevState, ...data.users]);
+    })();
+  }, []);
+
+  const handleItemClick = () => {
+    setIsModalOpen(!isModalOpen);
+  };
 
   return (
     <>
       <SearchBar />
-      <List data={items} />
+
+      <>
+        <List data={items} isLoading={isLoading} onClick={handleItemClick} />
+        {isModalOpen &&
+          createPortal(
+            <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(!isModalOpen)} />,
+            document.body
+          )}
+      </>
     </>
   );
 };
