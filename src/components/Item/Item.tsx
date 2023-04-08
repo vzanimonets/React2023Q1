a906791bfa23cf46903296053711c3533bf85b5c
Viktar Zanimonets <vzanimonets@gmail.com>
2023-04-08 23:47:57 +0300

    refactor: change render for FormPage

--- src/components/Item/Item.tsx
+++ src/components/Item/Item.tsx
@@ -4,28 +4,14 @@ import LikeBtn from '../../assets/images/like.svg';
 
 type ItemPropsType = {
   id: string;
-  title?: string;
-  firstName?: string;
-  lastName?: string;
-  age?: number;
-  description?: string;
+  title: string;
+  description: string;
   image: string;
-  status?: string;
-  delivery?: string;
-  onClick?: () => void;
+  status: string;
+  delivery: string;
 };
 
-const Item = ({
-  firstName,
-  lastName,
-  title,
-  age,
-  description,
-  image,
-  status,
-  delivery,
-  onClick,
-}: ItemPropsType) => {
+const Item = ({ title, description, image, status, delivery }: ItemPropsType) => {
   const bgStyle = {
     backgroundImage: `url(${image})`,
   };
@@ -33,26 +19,18 @@ const Item = ({
     <div className={styles.card}>
       <div className={styles.card__image} style={bgStyle}></div>
       <div className={styles.card__content}>
-        <div className={styles.card__title}>
-          {title ? title : `${firstName} ${lastName},${age}`}
+        <div className={styles.card__title}>{title}</div>
+        <div className={styles.card__text}>{description}</div>
+        <div>
+          <label>Status: </label>
+          <i>{status}</i>
         </div>
-        {description && <div className={styles.card__text}>{description}</div>}
-        {status && (
-          <div>
-            <label>Status: </label>
-            <i>{status}</i>
-          </div>
-        )}
-        {delivery && (
-          <div>
-            <label>Delivery: </label>
-            <i>{delivery}</i>
-          </div>
-        )}
+        <div>
+          <label>Delivery: </label>
+          <i>{delivery}</i>
+        </div>
         <div className={styles.card__btns}>
-          <button className={styles.btn} onClick={onClick}>
-            Show Details
-          </button>
+          <button className={styles.btn}>Show Details</button>
           <img src={LikeBtn} className={styles.icon__like} alt="" />
           <span className={styles.like__count}>324</span>
         </div>
--- src/pages/FormPage.tsx
+++ src/pages/FormPage.tsx
@@ -1,18 +1,27 @@
 import React, { FC, useState } from 'react';
 import Form from '../components/Form/Form';
-import { ItemType, ShotInfoType } from '../components/App/App';
-import List from '../components/List/List';
+import { ItemType } from '../components/App/App';
+import styles from '../components/List/list.module.css';
+import Item from '../components/Item/Item';
 
 const FormPage: FC = () => {
-  const [items, setItems] = useState<ShotInfoType[]>([]);
+  const [items, setItems] = useState<ItemType[]>([]);
 
   const addItem = (newItem: ItemType) => {
     setItems([newItem, ...items]);
   };
   return (
     <>
-      <Form addItem={addItem} />;
-      <List data={items} isLoading={false} onClick={() => {}} />
+      <Form addItem={addItem} />
+      <ul className={styles.cards}>
+        {items.map((item) => {
+          return (
+            <li className={styles.cards__item} key={item.id}>
+              <Item {...item} />
+            </li>
+          );
+        })}
+      </ul>
     </>
   );
 };
