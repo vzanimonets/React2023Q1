f2430ed90a2710f9f169d885e95ef09c010bb60e
Viktar Zanimonets <vzanimonets@gmail.com>
2023-04-09 00:12:34 +0300

    fix: remove DetailModal from component

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,10 @@
 import React, { FC, useEffect, useState } from 'react';
-import { createPortal } from 'react-dom';
 import SearchBar from '../components/SearchBar/SearchBar';
 import List from '../components/List/List';
-import Modal from '../components/Modal/Modal';
 import { ShotInfoType } from '../components/App/App';
 import { DataAPI } from '../services/dataAPI';
 
 const HomePage: FC = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
   const [items, setItems] = useState<ShotInfoType[]>([]);
   const [isLoading, setIsLoading] = useState(false);
 
@@ -21,21 +18,12 @@ const HomePage: FC = () => {
     })();
   }, []);
 
-  const handleItemClick = () => {
-    setIsModalOpen(!isModalOpen);
-  };
-
   return (
     <>
       <SearchBar />
 
       <>
-        <List data={items} isLoading={isLoading} onClick={handleItemClick} />
-        {isModalOpen &&
-          createPortal(
-            <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(!isModalOpen)} />,
-            document.body
-          )}
+        <List data={items} isLoading={isLoading} />
       </>
     </>
   );
